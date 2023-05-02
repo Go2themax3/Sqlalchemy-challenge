24fae13562f8b0b4ec1438e8b86878c5bb4a048a
Max Young <go2themax99@gmail.com>
2023-05-02 13:37:52 -0400

    Final code

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def welcome():
-    """List all available api routes."""
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
@@ -48,13 +47,10 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    """Return a dictionary of dates and precipitation"""
-    # Query for the last 12 months of precipitation data
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
     one_year_ago = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=365)
     precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all()
     
-    # Convert the query results to a dictionary
     precipitation_dict = {}
     for date, prcp in precipitation_data:
         precipitation_dict[date] = prcp
@@ -63,7 +59,6 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    """Return a list of stations"""
     stations_data = session.query(Station.station).all()
     stations_list = list(np.ravel(stations_data))
     
@@ -71,8 +66,6 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    """Return a list of temperature observations for the previous year"""
-    # Query for the last 12 months of temperature observation data
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
     one_year_ago = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=365)
     temperature_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= one_year_ago).all()
@@ -86,10 +79,7 @@ def tobs():
 
 @app.route("/api/v1.0/<start>")
 def start_date(start):
-    """Return a list of minimum, average, and maximum temperatures for a given start date"""
-    # Query for the minimum, average, and maximum temperatures for a given start date
     start_date_temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).all()
     
-    # Convert the query results to a list
     start_temps_list = list(np.ravel(start_date_temps))
